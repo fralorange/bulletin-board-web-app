64458dbeb237994bbab48b9041d96c7f764c9171
fralorange <fralorange@mail.ru>
2023-08-25 02:47:31 +0300

    Add: Basic contracts (AdDto, CreateAdDto, AttachmentDto) and LimitAttribute

---
+++ src/Contracts/BulletinBoard.Contracts/Ad/AdDto.cs
@@ -0,0 +1,36 @@
+﻿using BulletinBoard.Contracts.Attachment;
+using BulletinBoard.Contracts.Base;
+
+namespace BulletinBoard.Contracts.Ad
+{
+    /// <summary>
+    /// Объявление.
+    /// </summary>
+    public class AdDto : BaseDto
+    {
+        /// <summary>
+        /// Заголовок.
+        /// </summary>
+        public required string Title { get; init; }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public required string Description { get; init; }
+
+        /// <summary>
+        /// Название категории.
+        /// </summary>
+        public required string CategoryName { get; init; }
+
+        /// <summary>
+        /// Изображения.
+        /// </summary>
+        public required IReadOnlyCollection<AttachmentDto> Attachments { get; init; }
+
+        /// <summary>
+        /// Цена.
+        /// </summary>
+        public required decimal Price { get; init; }
+    }
+}
---
+++ src/Contracts/BulletinBoard.Contracts/Ad/CreateAdDto.cs
@@ -0,0 +1,44 @@
+﻿using BulletinBoard.Contracts.Attachment;
+using BulletinBoard.Contracts.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulletinBoard.Contracts.Ad
+{
+    /// <summary>
+    /// Создание объявления.
+    /// </summary>
+    public class CreateAdDto
+    {
+        /// <summary>
+        /// Заголовок.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50)]
+        public string Title { get; init; }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(150)]
+        public string Description { get; init; }
+
+        /// <summary>
+        /// Идентификатор категории.
+        /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        public Guid CategoryId { get; init; }
+
+        /// <summary>
+        /// Изображения.
+        /// </summary>
+        [Limit(1, 5)]
+        public required IReadOnlyCollection<AttachmentDto> Attachments { get; init; }
+
+        /// <summary>
+        /// Цена.
+        /// </summary>
+        [Range(0, double.MaxValue)]
+        public required decimal Price { get; init; }
+    }
+}
