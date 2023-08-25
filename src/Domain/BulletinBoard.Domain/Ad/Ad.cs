6aae796a832110b03340b42e7008052678b98d69
fralorange <fralorange@mail.ru>
2023-08-25 20:50:48 +0300

    Fix: Fix namespaces

---
+++ src/Domain/BulletinBoard.Domain/Ad/Ad.cs
@@ -0,0 +1,40 @@
+﻿using BulletinBoard.Domain.Base;
+
+namespace BulletinBoard.Domain.Ad
+{
+    /// <summary>
+    /// Объявление.
+    /// </summary>
+    public class Ad : BaseEntity
+    {
+        /// <summary>
+        /// Заголовок.
+        /// </summary>
+        public required string Title { get; init; }
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public required string Description { get; init; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public required Guid CategoryId { get; init; }
+
+        /// <summary>
+        /// Категория.
+        /// </summary>
+        public Category.Category Category { get; init; }
+
+        /// <summary>
+        /// Изображения.
+        /// </summary>
+        public required IReadOnlyCollection<Attachment.Attachment> Attachments { get; init; }
+
+        /// <summary>
+        /// Цена.
+        /// </summary>
+        public required decimal Price { get; init; }
+    }
+}
---
+++ src/Domain/BulletinBoard.Domain/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace BulletinBoard.Domain.Base
+{
+    /// <summary>
+    /// Базовая сущность.
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
