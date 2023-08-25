9b390a02a3d4091111af4c1b082d0b65dea2a997
fralorange <fralorange@mail.ru>
2023-08-25 05:05:23 +0300

    Add: Domains

---
+++ src/Domain/BulletinBoard.Domain/Ad/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace BulletinBoard.Domain.Ad
+{
+    /// <summary>
+    /// Базовая сущность.
+    /// </summary>
+    public class BaseEntity
+    {
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Domain/BulletinBoard.Domain/Attachment/Attachment.cs
@@ -0,0 +1,15 @@
+﻿using BulletinBoard.Domain.Ad;
+
+namespace BulletinBoard.Domain.Attachment
+{
+    /// <summary>
+    /// Вложение.
+    /// </summary>
+    public class Attachment : BaseEntity
+    {
+        /// <summary>
+        /// Содержимое вложения в виде массива байтов.
+        /// </summary>
+        public required byte[] Content { get; init; }
+    }
+}
