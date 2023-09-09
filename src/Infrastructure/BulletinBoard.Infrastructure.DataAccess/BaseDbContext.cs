6429bc3a6a95b2ada3af2f3fa8848ce1915aed0f
fralorange <fralorange@mail.ru>
2023-09-09 20:12:57 +0300

    Add: BaseDbContext, AdConfiguration & IDbContextOptionsConfigurator

---
+++ src/Infrastructure/BulletinBoard.Infrastructure.DataAccess/BaseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulletinBoard.Infrastructure.DataAccess.Contexts.Ad.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoard.Infrastructure.DataAccess
+{
+    /// <summary>
+    /// Контекст БД.
+    /// </summary>
+    public class BaseDbContext : DbContext
+    {
+        /// <inheritdoc/>
+        public BaseDbContext(DbContextOptions options) : base(options) { }
+
+        /// <inheritdoc/>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AdConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
