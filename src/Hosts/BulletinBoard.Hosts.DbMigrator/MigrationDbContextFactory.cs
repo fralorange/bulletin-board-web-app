b3350520fc6a0c14d7152177ed0e2153a8b8167f
fralorange <fralorange@mail.ru>
2023-09-09 20:14:05 +0300

    Add: DbMigrator

---
+++ src/Hosts/BulletinBoard.Hosts.DbMigrator/MigrationDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BulletinBoard.Hosts.DbMigrator
+{
+    /// <summary>
+    /// Фабрика контекста БД для мигратора.
+    /// </summary>
+    public class MigrationDbContextFactory : IDesignTimeDbContextFactory<MigrationDbContext>
+    {
+        /// <inheritdoc cref="IDesignTimeDbContextFactory{TContext}"/>
+        public MigrationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("PostgresBoardDb");
+
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<MigrationDbContext>();
+            dbContextOptionsBuilder.UseNpgsql(connectionString);
+
+            return new MigrationDbContext(dbContextOptionsBuilder.Options);
+        }
+    }
+}
