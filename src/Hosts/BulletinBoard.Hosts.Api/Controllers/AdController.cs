c8f2ed309c81b48cab49d60e532d407a2b781c8e
fralorange <fralorange@mail.ru>
2023-10-16 19:37:47 +0300

    Doc: Update XML-Doc in AdController

--- src/Hosts/BulletinBoard.Hosts.Api/Controllers/AdController.cs
+++ src/Hosts/BulletinBoard.Hosts.Api/Controllers/AdController.cs
@@ -44,7 +44,7 @@ namespace BulletinBoard.Hosts.Api.Controllers
         }
 
         /// <summary>
-        /// Возвращает объявлению по заданному идентификатору.
+        /// Возвращает объявления по заданному идентификатору.
         /// </summary>
         /// <remarks>
         /// Пример: curl -X 'GET' \'https://localhost:port/ad/get-by-id'
